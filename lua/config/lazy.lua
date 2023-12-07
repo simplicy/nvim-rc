c89e672b4cf69def3865e00afd605b6ca26c0c15
Sean Hopkins <ratchetclnk55@gmail.com>
2023-12-07 06:31:56 -0500

    updated theme

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -28,6 +28,9 @@ require("lazy").setup({
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
   checker = { enabled = true }, -- automatically check for plugin updates
+  ui = {
+    border = "single",
+  },
   performance = {
     rtp = {
       -- disable some rtp plugins
