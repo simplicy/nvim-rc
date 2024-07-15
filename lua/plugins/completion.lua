c6de09b0683cf1cb4b0dd60259bfb6d4f06db9d0
Sean Hopkins <shopkins@mapcommunications.com>
2024-07-15 16:49:16 -0400

    updated completion, bug with program itself

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -8,7 +8,7 @@ return {
       table.insert(opts.sources, { name = "emoji" })
     end,
   },
-    -- Use <tab> for completion and snippets (supertab)
+  -- Use <tab> for completion and snippets (supertab)
   -- first: disable default <tab> and <s-tab> behavior in LuaSnip
   {
     "L3MON4D3/LuaSnip",
@@ -60,3 +60,4 @@ return {
     end,
   },
 }
+
