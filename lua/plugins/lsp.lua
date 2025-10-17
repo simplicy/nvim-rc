7677d3a57198835d17636b1cde016ee5112e02cd
Simplicy <ratchetclnk55@gmail.com>
2025-10-17 11:25:33 -0400

    updated mason

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,9 +11,10 @@ return {
     },
   },
   -- add any tools you want to have installed below
-  { "mason-org/mason-lspconfig.nvim" },
+  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
   {
     "mason-org/mason.nvim",
+    version = "^1.0.0",
     opts = {
       ensure_installed = {
         "stylua",
